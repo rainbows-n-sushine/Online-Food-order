3049bfaef67ab5d7c9725909cdc67ea85df64086
Jalal Addisu <jayaddisu@gmail.com>
2024-12-09 22:15:31 +0300

    Adding vendor model

--- food-order-backend/controllers/AdminController.ts
+++ food-order-backend/controllers/AdminController.ts
@@ -1,5 +1,6 @@
 import {Request,Response,NextFunction} from "express"
 import {CreateVendorInput} from "../dto"
+import {Vendor} from '../models'
 
 
 export const CreateVendor=async(req:Request,res:Response,next:NextFunction)=>{
@@ -7,6 +8,22 @@ export const CreateVendor=async(req:Request,res:Response,next:NextFunction)=>{
     try{
         const {name, ownerName, email ,foodType, pincode, address,phone, password}= <CreateVendorInput>req.body;
         // console.log(req.body)
+        const createdVendor= await Vendor.create({
+            name:name,
+            ownerName:ownerName, 
+            email:email ,
+            foodType:foodType, 
+            pincode:pincode, 
+            address:address,
+            phone:phone, 
+            password:password,
+            rating:0,
+            serviceAvailable:false,
+            salt:"",
+            coverImages:[]
+
+
+        })
         res.send({name, ownerName, email ,foodType, pincode, address,phone, password})
         return;
 

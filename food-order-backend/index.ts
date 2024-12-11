e2014835f0b39d3d96c097799d762c50ebe4e8aa
Jalal Addisu <jayaddisu@gmail.com>
2024-12-11 13:14:02 +0300

    Just made mongoose connect.

--- food-order-backend/index.ts
+++ food-order-backend/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import {AdminRouter, VendorRouter} from "./routes";
 import bodyParser from "body-parser";
+import mongoose,{ConnectOptions} from "mongoose";
+import {MONGO_URI} from "./config"
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -12,6 +14,17 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.use("/admin", AdminRouter);
 app.use('/vendor',VendorRouter);
+ 
+mongoose.connect(MONGO_URI,{
+  autoIndex:true,
+  autoCreate:true,
+  user:'jayaddisu',
+  pass:"jayaddisu5353"
+}).then((result)=>{
+  console.log("Mongoose is connected :)")
+}).catch((err)=>{
+  console.log('error ',err)
+})
 
 app.listen(PORT, () => {
   console.clear();

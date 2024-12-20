5a3332d39911edc716ba1a053d238f5566b672c6
Jalal Addisu <jayaddisu@gmail.com>
2024-12-20 16:03:22 +0300

    Get restaurants by Id controller done

--- food-order-backend/controllers/ShoppingController.ts
+++ food-order-backend/controllers/ShoppingController.ts
@@ -48,8 +48,36 @@ if(result.length>0){
 
 }
 export const SearchFoods=async(req:Request,res:Response,next:NextFunction)=>{
+    const pincode=req.params.pincode
+    const result=await Vendor.find({pincode:pincode,serviceAvailable:false})
+    .populate('foods')
+
+    if(result.length>0){
+        let foodResult:any=[];
+        result.map((vendor)=>{
+            const food=vendor.foods
+            foodResult.push(...food)
+
+        })
+        res.status(200).send(foodResult);
+        return;
+
+    }
+    res.status(400).send({message:"Data not found"})
+
 
 }
 export const RestaurantsById=async(req:Request,res:Response,next:NextFunction)=>{
+    const {pincode}=req.params
+    const {id}=req.params
+    const result=await Vendor.findById(id).populate('foods')
+
+    if(result){
+        res.status(200).send(result);
+        return;
+
+    }
+    res.status(400).send({message:"Data not found"})
+    return;
 
 }

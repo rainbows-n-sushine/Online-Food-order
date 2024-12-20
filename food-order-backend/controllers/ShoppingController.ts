c1b70a32d3812ec7d51d098c73a52fb682353b2a
Jalal Addisu <jayaddisu@gmail.com>
2024-12-20 11:15:14 +0300

    Get Top restaurants controller added

--- food-order-backend/controllers/ShoppingController.ts
+++ food-order-backend/controllers/ShoppingController.ts
@@ -9,7 +9,6 @@ export const GetFoodAvailability=async(req:Request,res:Response,next:NextFunctio
     .populate('foods')
     if(result.length>0){
         res.status(200).send(result);
-        console.log('this is foods in get food availability: ', result)
         return;
     }
    res.status(400).json({"message":"Data not found"})
@@ -17,6 +16,17 @@ export const GetFoodAvailability=async(req:Request,res:Response,next:NextFunctio
 
 }
 export const GetTopRestaurants=async(req:Request,res:Response,next:NextFunction)=>{
+    const pincode=req.params.pincode;
+    const result= await Vendor.find({pincode:pincode, serviceAvailable:false})
+    .sort([["rating","descending"]])
+    .limit(1)
+    if(result.length>0){
+        res.status(200).send(result);
+        console.log('This is the top 1 restaurant: ', result)
+        return;
+    }
+   res.status(400).json({"message":"Data not found"})
+   return;
 
 }
 export const GetFoodsIn30mins=async(req:Request,res:Response,next:NextFunction)=>{
